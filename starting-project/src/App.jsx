001a3775d1d5c0ee4dffd3158f08ba30442498d9
ISHA-PL\Hp <tejaswi.rkariya@gmail.com>
2025-07-30 23:05:18 +0530

    dynamic values

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,12 +1,20 @@
+const reactDescription = ["Fundamental", "Core", "Crucial"]
+
+function getRandomInt(max){
+  return Math.floor(Math.random() * (max + 1))
+}
 
 // create component , always starts with capital latter
 function Header() {
+
+  const description = reactDescription[getRandomInt(2)]
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
